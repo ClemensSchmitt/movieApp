c7c438d99c55e5e4bca37b367d3b055d959947d4
Julius <julius@bauernhof-nielsen.de>
2022-01-04 15:03:37 +0100

    Login Update 2

--- components/Login.js
+++ components/Login.js
@@ -13,8 +13,6 @@ const Login = () => {
             <View style={styles.loginMask}>
                 <TextInput 
                 style={styles.userEmail}
-                onChangeText={onChangeNumber}
-                value={number}
                 //onChangeText={onChangeNumber}
                 //value={number}
                 placeholder="Email"
@@ -24,9 +22,6 @@ const Login = () => {
                 </TextInput>
                 <TextInput 
                 style={styles.userPassword}
-                onChangeText={onChangeNumber}
-                value={number}
-                placeholder="Passwort"
                 //onChangeText={onChangeNumber}
                 //value={number}
                 placeholder="Password"
@@ -45,42 +40,36 @@ const Login = () => {
     
 const styles = StyleSheet.create({
     container: {
-        container: {
-        flex: 1,
-        padding: 20,
-        flexDirection: "column",
-        },
-        logoPicture:{
-        flex: 1,
-        },
-        loginMask: {
-        flex: 1,
-        flexDirection: "column",
-        },
-        userEmail:{
-        flex:1,
-        backroundColor:'#6200EA', 
-        backgroundColor:'#6200EA', 
-        borderRadius: 8,
-        padding: 10,
-        margin: 5
-      
-        }, 
-        userPasswort:{
-        userPassword:{
-        flex:1,
-        backroundColor:'#6200EA',
-        backgroundColor:'#6200EA',
-        borderRadius: 8,
-        padding: 10,
-        margin: 5
-        },
-        spacer:{
-        flex:1,
-        }
-      
+    flex: 1,
+    padding: 20,
+    flexDirection: "column",
+    },
+    logoPicture:{
+    flex: 1,
+    },
+    loginMask: {
+    flex: 1,
+    flexDirection: "column",
+    },
+    userEmail:{
+    flex:1,
+    backgroundColor:'#6200EA', 
+    borderRadius: 8,
+    padding: 10,
+    margin: 5
+    
+    }, 
+    userPassword:{
+    flex:1,
+    backgroundColor:'#6200EA',
+    borderRadius: 8,
+    padding: 10,
+    margin: 5
+    },
+    spacer:{
+    flex:1,
     }
-);
-    });
+
+});
 
 export default Login;
