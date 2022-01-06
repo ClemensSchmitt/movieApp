da01b83ef53f4e5cbf34125567875bbe53527b6d
Julius <julius@bauernhof-nielsen.de>
2022-01-06 16:30:57 +0100

    Profile 1

---
+++ components/Profile.js
@@ -0,0 +1,101 @@
+import React from "react";
+import { StyleSheet, Text, View, TouchableOpacity, Alert, TextInput, Pressable} from 'react-native';
+
+const Profile = ({navigation}) => {
+
+    return (
+        <View style={styles.container}>
+
+            <View style={styles.header}>
+
+                <Text style={styles.headerTextStyle}>Profile</Text>
+
+            </View>
+
+
+            <View style={styles.buttonContainer}>
+                <Pressable onPress = {() => navigation.navigate("Favorites")} title='Favorites' style={styles.buttonStyle}>
+                <Text style={styles.buttonTextStyle}>Favorites</Text>
+                </Pressable>
+            </View>
+
+            <View style={styles.buttonContainer}>
+                <Pressable onPress = {() => navigation.navigate("PersonalRanking")} title='PersonalRanking' style={styles.buttonStyle}>
+                <Text style={styles.buttonTextStyle}>PersonalRanking</Text>
+                </Pressable>
+            </View>
+
+            <View style={styles.spacer}>
+                
+            </View>
+
+            <View style={styles.buttonContainer}>
+        
+            </View>
+
+            <View style={styles.spacer}>
+                
+            </View>
+            <View style={styles.buttonContainer}>
+                <Pressable onPress = {() => navigation.navigate("Impressum")} title='Impressum' style={styles.buttonStyle}>
+                <Text style={styles.buttonTextStyle}>Impressum</Text>
+                </Pressable>
+            </View>
+
+
+
+
+
+
+        </View>
+    )
+}
+
+    
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        flexDirection: "column",
+    },
+
+    headerTextStyle: {
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        color: '#FFF',
+        top: 15,
+        textShadowColor: '#000000',
+        textShadowRadius: 10,
+        flex: 1,
+    },
+
+    spacer:{
+        flex:1,
+    },
+
+    buttonStyle: {
+        backgroundColor: '#6200EA',
+        height: 50,
+
+        borderRadius: 8,
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 12,
+        },
+        shadowOpacity: 0.58,
+        shadowRadius: 16.00,
+        elevation: 12,
+      },
+    buttonTextStyle: {
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        color: '#FFF',
+        top: 15,
+        textShadowColor: '#000000',
+        textShadowRadius: 10,
+      },
+
+});
+
+export default Profile;
