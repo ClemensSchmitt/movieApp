9568a76cc5fb1df21cf2f2b43840ae9f68a55942
Clemens Schmitt <Clemens.schmitt98@gmail.com>
2022-01-08 16:14:33 +0100

    Login improved

--- components/Login.js
+++ components/Login.js
@@ -1,20 +1,24 @@
 import React, {useState} from "react";
 import { StyleSheet, Text, View, TouchableOpacity, Alert, TextInput, Pressable} from 'react-native';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getDatabase, ref, child, onValue} from "firebase/database";
+import { get, set } from "firebase/database";
+import firebase from "../firebase";
 
 const Login = ({navigation}) => {
-
 
     const [state, setState] = useState({
       email: "",
       password: "",
+      id: "",
     });
-
 
     const textChange = (name, value) => {
       setState({...state, [name]: value})
     };
 
     const login = () => {
+      getNewId();
       if(state.email == ''){
         Alert.alert("Please enter an email");
       }
@@ -22,7 +26,67 @@ const Login = ({navigation}) => {
         Alert.alert("Please enter your password");
       }
       else{
-        navigation.navigate("Dashboard");
+        if(isUserRegistered()){
+          navigation.navigate("Dashboard");
+        }
+      }
+    }
+
+    const getNewId = () => {
+      get(child(ref(firebase.db), 'users/userData'))
+        .then((snapshot) => {
+          if(snapshot.exists()){
+            var id = 1;
+            var data = snapshot.val();
+            for(var entry in data){
+              ++id;
+            }
+            setState({...state, ["id"]: id.toString()});
+          }
+          else{
+            var id = 1;
+            setState({...state, ["id"]: id.toString()});
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+
+    const [requestedDBState, setRequestedDBState] = useState({
+      email: "",
+      password: "",
+    });
+
+    //requestedDBState.email wird noch nicht richtig gesetzt
+    function isUserRegistered() {
+      for(var i = 1; i < state.id; i++){
+        get(child(ref(firebase.db), 'users/userData/' + i.toString() + "/email"))
+        .then((snapshot) => {
+          if(snapshot.exists()){
+            //snapshot.val() liefert das richtige ergebnis
+            setRequestedDBState({...requestedDBState, ["email"]: snapshot.val()});
+            Alert.alert(requestedDBState.email);
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+        get(child(ref(firebase.db), 'users/userData/' + i.toString() + "/password"))
+        .then((snapshot) => {
+          if(snapshot.exists()){
+            setRequestedDBState({...requestedDBState, ["password"]: snapshot.val()});
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+        
+        if(requestedDBState.password === state.password && requestedDBState.email === state.email){
+
+          Alert.alert(state.password + "" + requestedDBState.password);
+          return true;
+        }
       }
     }
 
